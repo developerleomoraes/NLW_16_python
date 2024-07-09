909d418b3431eb64d95b155d4b731c5b82255fba
Leo Moraes <developerleo.it@gmail.com>
2024-07-09 00:03:17 -0300

    Create first route

---
+++ src/main/routes/trips_routes.py
@@ -0,0 +1,8 @@
+from flask import jsonify, Blueprint
+
+trips_routs_bp = Blueprint("trip_routes", __name__)
+
+@trips_routs_bp.route("/trips", methods=["POST"])
+
+def create_trip():
+    return jsonify({'Hello': 'World'}), 200
--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from src.main.routes.trips_routes import trips_routs_bp
 
 
 app = Flask(__name__)
+
+app.register_blueprint(trips_routs_bp)
